4e51dae916eea53c5741f79cee4470ee2763b63d
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-05-30 14:42:15 +0530

    some

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import { BrowserRouter,Route, Routes } from 'react-router-dom'
 import LandingPage from './components/LandingPage/LandingPage'
-import Login from './components/LoginAndSignUp/Login'
 import SignUp from './components/LoginAndSignUp/SignUp'
 function App() {
 

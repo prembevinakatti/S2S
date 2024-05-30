c1b52ad328a50f3d811f496fe4fba86df814a4f9
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-05-30 12:36:49 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import Input from './components/InputComponent/Input'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Input placeholder="hello" />
     </>
   )
 }

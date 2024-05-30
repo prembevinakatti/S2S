f1f12cf458b976a103e67246888245568eb1c2f7
vishunstopabel <135271299+vishunstopabel@users.noreply.github.com>
2024-05-30 07:02:58 +0000

    changes in in put

--- src/components/inputscomponents/input.jsx
+++ src/components/inputscomponents/input.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { forwardRef } from 'react'
-const Input = ({ref}) => {
+import React, { forwardRef } from 'react';
+
+const Input = forwardRef((props, ref) => {
   return (
     <div>
-      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" ref={ref} />
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" ref={ref} {...props} />
     </div>
-  )
-}
+  );
+});
 
-export default forwardRef.Input
+export default Input;

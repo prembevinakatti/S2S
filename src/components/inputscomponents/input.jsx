b833f90a950431ab774747a36bf77eddc8cbe6e9
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-05-30 12:24:15 +0530

    added input box

---
+++ src/components/inputscomponents/input.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+    </div>
+  )
+}
+
+export default Input

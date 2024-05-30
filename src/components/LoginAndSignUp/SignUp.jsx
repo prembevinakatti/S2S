10edfd4221dc5218d84805f773af4318931f19c2
vishunstopabel <135271299+vishunstopabel@users.noreply.github.com>
2024-05-30 09:10:17 +0000

    added logic  of signup

--- src/components/LoginAndSignUp/SignUp.jsx
+++ src/components/LoginAndSignUp/SignUp.jsx
@@ -1,17 +1,40 @@
 import React from 'react'
 import Input from "../InputComponent/Input";
-
+import { useForm } from 'react-hook-form';
+import authService from '../../appwrite/services';
 const SignUp = () => {
+  const {register,handleSubmit}=useForm()
+  function handelsignup(data){
+    try {
+      if (data){
+        authService.createAccount(...data).then((authdata)=>{
+          if(authdata){
+            authService.getCurrentUser().then((userdata)=>{
+                console.log( userdata)
+                
+            }).catch((error)=>{
+              console.log(error)
+            })
+          }
+        }).catch((error)=>{
+          console.log(error)
+        })
+    }
+    } catch (error) {
+      
+    }
+     
+  }
   return (
     <div className="w-full h-full flex items-center justify-center">
       <div className="RegisterPage flex flex-col items-center p-2 w-[30vw] h-fit border border-slate-500 rounded-lg">
         <img className="w-14 mb-5" src="src/assets/S2SLogo.png" alt="" />
-        <form className="flex gap-5 flex-col items-center">
+        <form className="flex gap-5 flex-col items-center" onSubmit={handleSubmit(handelsignup)}>
             <h2 className="text-3xl text-white">Sign Up</h2>
-          <Input placeholder="Res Name" />
-          <Input placeholder="Email" />
-          <Input placeholder="Phone Number" />
-          <Input placeholder="Password" />
+          <Input placeholder="Res Name"{...register("name")} />
+          <Input placeholder="Email"{...register("email")} />
+          <Input placeholder="Phone Number"{...register("phonenumber")} />
+          <Input placeholder="Password"{...register("password")} />
           <div className="divider">OR</div>
             <button className="btn w-[20vw]"><img className="w-7" src="src/assets/google.png" alt="" />Continue with Google</button>
             <button className="btn w-[20vw] btn-active btn-primary">Sign Up</button>

8d546aa0bd1f99d1891018c002863373539254d7
vishunstopabel <135271299+vishunstopabel@users.noreply.github.com>
2024-05-30 06:55:47 +0000

    add ref

--- src/components/inputscomponents/input.jsx
+++ src/components/inputscomponents/input.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
-const Input = () => {
+import { forwardRef } from 'react'
+const Input = ({ref}) => {
   return (
     <div>
-      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" ref={ref} />
     </div>
   )
 }
 
-export default Input
+export default forwardRef.Input

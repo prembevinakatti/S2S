ff02be6517155677bb3a391687f4a13d4703e259
vishunstopabel <135271299+vishunstopabel@users.noreply.github.com>
2024-05-31 18:16:51 +0000

    upload page functionnality

--- src/components/UploadPage/UploadPage.jsx
+++ src/components/UploadPage/UploadPage.jsx
@@ -2,24 +2,82 @@ import React from "react";
 import Input from "../InputComponent/Input";
 import TextArea from "../TextArea";
 import DropDown from "../DropDown";
+import { useForm } from "react-hook-form";
+import { ID } from "appwrite";
+import { useSelector } from "react-redux";
+import profileService from "../../appwrite/profile";
+const UploadPage = (editdata) => {
+  const authdata = useSelector((state) => state.auth.userData);
+  const {register,handleSubmit} =useForm()
+  async function  handeluplod(data){
+    if(editdata){
+      try {
+        data.slug=editdata.$id
+        data.userId=authdata.$id
+        const imageid=[]
+        data.images.map((image,index)=>{
+           const data=async()=>(await profileService.uploadFile(image))
+            if(data){
+              imageid.push(data)
+            }
 
-const UploadPage = () => {
+            if(imageid.length!=0){
+              data.imageids=JSON.stringify(imageid)
+              const uplodfile=async()=>(k)
+
+
+            }
+        })
+      } catch (error) {
+        
+      }
+    }
+    else{
+        try {
+          data.slug=ID.unique()
+          data.userId=authdata.$id
+          const imageid=[]
+          data.images.map((image,index)=>{
+             const data=async()=>(await profileService.uploadFile(image))
+              if(data){
+                imageid.push(data)
+              }
+
+              if(imageid.length!=0){
+                data.imageids=JSON.stringify(imageid)
+                const uplodfile=async()=>(k)
+
+
+              }
+          })
+        } catch (error) {
+          
+        }
+
+    }
+  }
   return (
+    <form action="" onSubmit={handleSubmit(handeluplod)}>
     <div className="UploadPage w-full h-full flex px-10 gap-10 items-center justify-evenly ">
       <div className="UploadImg w-[30vw] h-[70vh] m-2">
-        <img className="w-full h-full object-cover rounded-lg"
-          src="https://imgs.search.brave.com/fEvwJdYKaOMoBRa31i9328qtO4yj1b-3yAOeG_2U4jY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE0/NjcwMDM5MDk1ODUt/MmY4YTcyNzAwMjg4/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4bGVI/QnNiM0psTFdabFpX/UjhNVEI4Zkh4bGJu/d3dmSHg4Zkh3PQ.jpeg"
-          alt=""
-        />
+      <input
+                id="image-upload"
+                type="file"
+                className="hidden"
+                accept="image/png, image/jpg, image/jpeg, image/gif"
+                onChange={handlePreviewImage}
+                {...register("images")}
+              />
       </div>
       <div className="UploadDetails flex items-center flex-col justify-center gap-5">
-        <Input placeholder="Number Of People To Feed" />
-        <TextArea placeholder="Food Items" />
-        <Input placeholder="Location" />
-        <DropDown />
-        <button className="btn btn-outline w-[22vw] btn-primary">Upload</button>
+        <Input placeholder="Number Of People To Feed" {...register("nofood")} />
+        <TextArea placeholder="Food Items" {...register("details")} />
+        <Input placeholder="Location" {...register("location")} />
+        <DropDown {...register("modofdev")} />
+        <button className="btn btn-outline w-[22vw] btn-primary" type="submit">Upload</button>
       </div>
     </div>
+    </form>
   );
 };
 

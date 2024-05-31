a736dce2ad5f0ccf1c9f567197e0ffecb84c17a7
vishunstopabel <135271299+vishunstopabel@users.noreply.github.com>
2024-05-31 05:25:54 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import { useEffect } from 'react'
-import LandingPage from './components/LandingPage/LandingPage'
-import SignUp from './components/LoginAndSignUp/SignUp'
-import { useDispatch } from 'react-redux'
-import authService from './appwrite/services'
-import { login } from './store/authslice'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/services';
+import { login } from './store/authslice';
 
 function App() {
-  const dispatch =useDispatch()
-  useEffect(()=>{
-    try {
-      authService.getCurrentUser().then((data)=>{
-        if(data){
-          console.log(data)
-          dispatch(login(data))
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const userData = await authService.getCurrentUser();
+        if (userData) {
+          console.log(userData);
+          dispatch(login({userData}));
+      
         }
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  },[])
-  return (
-   <></>
-  )
+      } catch (error) {
+        console.log(error);
+        
+      }
+    };
+
+    fetchCurrentUser();
+  }, []);
+
+  return <></>;
 }
 
-export default App
+export default App;

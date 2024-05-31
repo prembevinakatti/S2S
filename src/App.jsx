9bb60c4da0f705b88fe4442c8c885b924b60e40d
vishunstopabel <135271299+vishunstopabel@users.noreply.github.com>
2024-05-31 05:00:56 +0000

    added app.jsx fun

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useEffect } from 'react'
 import LandingPage from './components/LandingPage/LandingPage'
 import SignUp from './components/LoginAndSignUp/SignUp'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/services'
+import { login } from './store/authslice'
+import { data } from 'autoprefixer'
 function App() {
-
+  const dispatch =useDispatch()
+  useEffect(()=>{
+    try {
+      authService.getCurrentUser().then(()=>{
+        if(data){
+          dispatch(login(data))
+        }
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  },[])
   return (
-   <div>
-     <LandingPage />
-     <SignUp />
-   </div>
+   <></>
   )
 }
 

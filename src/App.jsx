bd2ded3f3171d166942f16551de9f4d20ce03739
vishunstopabel <135271299+vishunstopabel@users.noreply.github.com>
2024-06-01 20:53:44 +0000

    .still

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,33 @@
 import { useEffect } from "react";
-import LandingPage from "./components/LandingPage/LandingPage";
-import SignUp from "./components/LoginAndSignUp/SignUp";
 import { useDispatch, useSelector } from "react-redux";
 import authService from "./appwrite/services";
 import { login } from "./store/authslice";
-import Outlate from "./components/Outlate";
-import ProfilePage from "./components/ProfilePage/ProfilePage";
-import Dashboard from "./components/Dashboard/Dashboard";
-import UploadPage from "./components/UploadPage/UploadPage";
-import PostPage from "./components/PostPage/PostPage";
-import Card from "./components/CardComp/Card";
-import HomePage from "./Pages/ResPage/ResHomePage";
 import profileService from "./appwrite/profile";
 import { updateProfile } from "./store/profuleslice";
-import NgoHomePage from "./Pages/NgoPage/NgoHomePage";
-import OrderDetailsPage from "./Pages/NgoPage/OrderDetailsPage";
-import ResLoginPage from "./Pages/ResPage/ResLoginPage";
-import ResRegPage from "./Pages/ResPage/ResRegPage";
-import NgoLoginPage from "./Pages/NgoPage/NgoLoginPage";
-import NgoRegPage from "./Pages/NgoPage/NgoRegPage";
-import ResProfilePage from "./Pages/ResPage/ResProfilePage";
-import NgoProfilePage from "./Pages/NgoPage/NgoProfilePage";
-import NgoDashboard from "./Pages/NgoPage/NgoDashboard";
 import ResDashboard from "./Pages/ResPage/ResDashboard";
+import { useNavigate } from "react-router-dom";
 function App() {
   const dispatch = useDispatch();
+  const navigate=useNavigate()
   useEffect(() => {
+    const profileData = useSelector((state) => state.profile.profiledata);
     try {
       authService.getCurrentUser().then((userData) => {
         if (userData) {
           dispatch(login({ userData }));
            profileService.getUser(userData.name).then((profiledata)=>{
+            
             dispatch(updateProfile({profiledata}))
+
            })
+           if(profileData==null){
+              if(profileData.ngoNumber==null||undefined||""){
+                navigate("ResProfilePage")
+              }
+              else{
+                navigate("NgoProfilePage")
+              }
+           }
          
         }
       });

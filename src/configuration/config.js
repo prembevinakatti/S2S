97b7f939fb34b7074084e571a257566828ecd3d0
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-06-01 10:45:46 +0530

    env

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -3,6 +3,7 @@ const conf={
     projectid:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     databaseid:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     collectionid:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    collectionid2:String(import.meta.env.VITE_APPWRITE_COLLECTION2_ID),
     storageid:String(import.meta.env.VITE_APPWRITE_STORAGE_ID),
 
 }

2eab4e54aec5a4305a36f4d79c1e7fac30930470
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-06-01 15:15:49 +0530

    asd

--- src/components/CardComp/Card.jsx
+++ src/components/CardComp/Card.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import CardNav from "./CardNav";
 import DetailsBox from "../DetailsBox";
 import CardDetails from "../CardDetails";
+import profileService from "../../appwrite/profile";
 
-const Card = () => {
+const Card = (props) => {
+  
   return (
     <div className="card shadow-xl bg-slate-700 gap-1 rounded-lg overflow-hidden w-[25vw] h-auto p-3">
       <CardNav />
       <div className="CardImg overflow-hidden w-full rounded-md h-[30vh]">
-        <img
+        {/* <img
           className="w-full h-full object-cover"
-          src="https://imgs.search.brave.com/0tky2yPxJM7n6eER8ZmhdJ0AdSSMOKMYARxzt6jDZbA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/NTk4NDc4NDQtNTMx/NTY5NWRhZGFlP3E9/ODAmdz0xMDAwJmF1/dG89Zm9ybWF0JmZp/dD1jcm9wJml4bGli/PXJiLTQuMC4zJml4/aWQ9TTN3eE1qQTNm/REI4TUh4elpXRnlZ/Mmg4TVRSOGZHUmxi/R2xqYVc5MWN5VXlN/R1p2YjJSOFpXNThN/SHg4TUh4OGZEQT0.jpeg"
+          // src={profileService.getFilePreview(props ? props.imageId : null)}
           alt=""
-        />
+        /> */}
       </div>
       <div className="CardContent flex flex-col gap-2">
-        <CardDetails details="Number Of People To Feed" />
-        <CardDetails details="Items" />
-        <CardDetails details="Mode Of Delivery" />
-        <CardDetails details="Details" />
+        <CardDetails details={props.nofeed} />
+        <CardDetails details={props.fooddetails} />
+        <CardDetails details={props.modofdev} />
       </div>
     </div>
   );

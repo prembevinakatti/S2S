1f70ae62dcf5fdee2546dc55dccee8a861e2f759
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-06-04 19:12:54 +0530

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -56,8 +56,8 @@ function App() {
   return (
     <>
       
-     {/* <Outlate />  */}
-     <FeedbackSection />
+     <Outlate /> 
+     {/* <FeedbackSection /> */}
      {/* <DistanceCalculator /> */}
      
     </>
